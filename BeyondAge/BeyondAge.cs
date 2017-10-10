5e709ff3fcdb957783bfd99c3361a3d4e786abc6
skyvaultgames <skyvaultgames@gmail.com>
2017-10-10 12:18:42 -0700

    We have a window!

---
+++ BeyondAge/BeyondAge.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BeyondAge
+{
+    public class BeyondAge : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch batch;
+        Texture2D test;
+
+        public static readonly int Width = 1280;
+        public static readonly int Height = 720;
+
+        public BeyondAge()
+        {
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = Width,
+                PreferredBackBufferHeight = Height
+            };
+
+            graphics.ApplyChanges();
+            
+            IsMouseVisible = true;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            batch = new SpriteBatch(GraphicsDevice);
+            test = Content.Load<Texture2D>("images/tilesheet_1");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            batch.Begin();
+
+            batch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
